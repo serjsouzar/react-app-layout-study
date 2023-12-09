7251502740ec02ace0ad9844267de1e2cad07261
Sergio Souza <serjsouza93@gmail.com>
2023-12-09 20:59:11 -0300

    products images

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BannerBackground, Container, Content, ImagesContent, SubtitlePresentation, TitlePresentation } from './styles'
 import doctorImage from "./../../assets/doctor-presenting-something-isolated-white-background 2.png";
+import ipadImage from "./../../assets/image 14.png"
+import iphoneImage from "./../../assets/image 15.png"
+import smartWatchImage from "./../../assets/image 16.png"
 
 const Banner = () => {
   return (
@@ -24,8 +27,17 @@ const Banner = () => {
         <div className='sphereOne'>
         </div>        
         <div className='sphereTwo'>        
+        <div className='ipadImage'>
+          <img src={ipadImage} alt='ipad-img'/>
         </div>
-        <img src={doctorImage} alt='doctor-image' />
+        <div className='iphoneImage'>
+        <img src={iphoneImage} alt='ipad-img'/>        
+        </div>
+        <div className='smartWatchImage'>
+        <img src={smartWatchImage} alt='ipad-img'/>                
+        </div>
+        </div>
+        <img src={doctorImage} className="doctorImg" alt='doctor-img' />
       </ImagesContent>      
     </Container>
   </BannerBackground>
--- src/components/Banner/styles.jsx
+++ src/components/Banner/styles.jsx
@@ -46,8 +46,9 @@ export const TitlePresentation = styled.div`
   h1 {
     font-size: 68px;
     font-style: normal;
-    font-weight: 700;
+    font-weight: 600;
     line-height: 85px;
+    font-family: Montserrat;    
 
     @media (max-width: 680px) {
       font-size: 36px;
@@ -62,8 +63,7 @@ export const TitlePresentation = styled.div`
     font-weight: 700;
     line-height: normal;
     width: 351px;
-    position: relative;
-    letter-spacing: -1px;
+    position: relative;    
   }
   h1,
   h2 {
@@ -125,14 +125,14 @@ export const ImagesContent = styled.div`
   height: 672px;
   transition: all ease 0.2s;
 
-  img {
+  .doctorImg {
     width: 624px;
     height: 672px;
     flex-shrink: 0;
     opacity: 1;
     position: relative;
     right: 50%;
-    padding-top:3px ;
+    padding-top: 3px;
     bottom: 11%;
     z-index: 8;
   }
@@ -155,8 +155,7 @@ export const ImagesContent = styled.div`
     top: 33px;
 
     border-radius: 727px;
-    opacity: 0.05;
-    background: #0078e6;
+    background: rgba(0, 120, 230, 0.05);
 
     @media (max-width: 680px) {
       width: 331.092px;
@@ -174,8 +173,7 @@ export const ImagesContent = styled.div`
     transition: all ease 0.2s;
 
     border-radius: 583.749px;
-    opacity: 0.1;
-    background: #0078e6;
+    background: rgba(0, 120, 230, 0.1);
     position: relative;
     z-index: 5;
 
@@ -185,6 +183,73 @@ export const ImagesContent = styled.div`
       flex-shrink: 0;
       top: 35px;
       left: 32px;
+    }
+  }
+
+  .ipadImage {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    border: 5px solid rgba(225, 237, 248, 1);
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow: 1px 1px 8px 5px rgba(0, 120, 230, 0.1);
+
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: 5px;
+    left: 45px;
+
+    img {
+      width: 64.364px;
+      height: 74.182px;
+      position: unset;
+    }
+  }
+
+  .iphoneImage {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    border: 5px solid rgba(225, 237, 248, 1);
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow: 1px 1px 8px 5px rgba(0, 120, 230, 0.1);
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    left: 82%;
+    bottom: 6%;
+
+    img {
+      width: 59.091px;
+      height: 82.955px;
+      position: unset;
+    }
+  }
+
+  .smartWatchImage {
+    width: 86px;
+    height: 86px;
+    flex-shrink: 0;
+    border-radius: 50%;
+    border: 3px solid rgba(225, 237, 248, 1);
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow: 1px 1px 8px 5px rgba(0, 120, 230, 0.1);
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    left:85%;
+    top:26%;
+
+    img {
+      width: 50px;
+      height: 50px;      
     }
   }
 `;
